6bad8d615a09e1fb6116c2a5ffa15d27c908ad17
Bastien <bastien_maurice@hotmail.com>
2019-11-09 13:51:19 +0100

    Welcome component responsive

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from '../welcome/welcome.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material';
+
 
 
 @NgModule({
     declarations: [WelcomeComponent],
     imports: [
         CommonModule,
-        WelcomeRoutingModule
+        WelcomeRoutingModule,
+        FlexLayoutModule,
+        MatButtonModule
     ]
 })
 export class WelcomeModule { }

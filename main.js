d557cc6412f995259bb8597cb2d3f83714f7a36b
Bastien <bastien_maurice@hotmail.com>
2019-11-09 10:09:41 +0100

    Up electron version + Fix toolbar issues click + responsive

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const { app, BrowserWindow } = require('electron')
 
+let win, isToolsDev;
+const args = process.argv.slice(1);
+isToolsDev = args.some(val => val === "--devTools");
+
 function createWindow () {
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+        nodeIntegration: true
     },
-    frame: false,
-    titleBarStyle: 'hiden'
+        frame: false,
+        titleBarStyle: 'hiden'
+    });
 
-  })
+    win.loadURL('http://localhost:4200/')
 
-  win.loadURL('http://localhost:4200/')
+    if(isToolsDev) {
+        win.webContents.openDevTools();
+    }
+
 }
 
 app.on('ready', createWindow)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
 import { WelcomeComponent } from './welcome-module/welcome.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
@@ -25,7 +25,9 @@ import { NgxElectronModule } from 'ngx-electron';
         MatToolbarModule,
         FlexLayoutModule,
         MatButtonModule,
-        NgxElectronModule
+        NgxElectronModule,
+        MatIconModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
